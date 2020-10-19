12d945f74cb87b272b3f605ff9f71cca74e2df0f
Cetrak <61994562+Cetrak@users.noreply.github.com>
2020-10-19 18:16:27 -0300

    Create contato.js

---
+++ ProjetoFinal/contato.js
@@ -0,0 +1,11 @@
+function contato(){
+    window.location.replace("https://www.filosofiaesoterica.com/contact/");
+}
+
+function Twitter(){
+    window.location.replace("https://twitter.com/FilosofiaEso")
+}
+
+function Facebook(){
+    window.location.replace("https://www.facebook.com/aFilosofiaEsoterica/?fref=ts")
+}
