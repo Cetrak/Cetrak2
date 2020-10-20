ad635783749a1b2590be6d83ee1a701dffe1fb8d
Cetrak <61994562+Cetrak@users.noreply.github.com>
2020-10-20 11:56:02 -0300

    Update contato.js

--- ProjetoFinal/contato.js
+++ ProjetoFinal/contato.js
@@ -1,5 +1,5 @@
 function contato(){
-    window.location.replace("https://www.filosofiaesoterica.com/contact/");
+    window.location.replace("https://cetrak.github.io/Exercicios-DW1A3/ProjetoFinal/contato.html");
 }
 
 function Twitter(){
@@ -8,4 +8,24 @@ function Twitter(){
 
 function Facebook(){
     window.location.replace("https://www.facebook.com/aFilosofiaEsoterica/?fref=ts")
+}
+
+function home(){
+    window.location.replace("https://www.filosofiaesoterica.com/#gsc.tab=0")
+}
+
+function grupo(){
+    window.location.replace("https://br.groups.yahoo.com/neo/groups/SerAtento/info")
+}
+
+function perfil(){
+    window.location.replace("https://www.filosofiaesoterica.com/perfil-da-loja-independente/")
+}
+
+function curso(){
+    window.location.replace("https://www.filosofiaesoterica.com/curso-discipulado-segundo-os-mestres/")
+}
+
+function mapas(){
+    window.location.replace("https://cetrak.github.io/Exercicios-DW1A3/ProjetoFinal/mapa.html")
 }
